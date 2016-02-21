8fcef9c8fadff7a52204243b23de22e0bdec3098
Gustavo Henrique <gustavo@gustavohenrique.net>
2016-02-21 12:05:45 -0300

    Added CORS support

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var restify = require('restify');
 
         var self = this;
         var HTTP_METHODS = ['del', 'get', 'head', 'options', 'patch', 'post', 'put'];
-
+        
         self.server = {};
         self.routes = {};
         self.jwtConfig = {
@@ -20,12 +20,21 @@ var restify = require('restify');
             return self;
         };
 
+        this.on = function (evt, func) {
+            self.server.on(evt, func);
+            return self;
+        };
+
         this.jwt = function (config) {
             checkIfServerIsDefined();
 
             if (! config || ! config.secretOrPrivateKey || ! config.callback) {
                 throw new Error('secretOrPrivateKey string and callback function are obrigatories.');
             }
+            if (config.cors) {
+                self.on('MethodNotAllowed', unknownMethodHandler);
+            }
+
             self.jwtConfig = config;
             self.server.use(authMiddleware);
 
@@ -34,11 +43,15 @@ var restify = require('restify');
                 if (httpVerb === 'delete') {
                     httpVerb = 'del'
                 }
+                if (httpVerb === 'options') {
+                    httpVerb = 'opts'
+                }
                 var key = req.route.path + '_' + httpVerb;
                 return self.routes[key].needsAuthentication;
             }
 
             function authMiddleware (req, res, next) {
+                
                 if (isPathNeedsAuthentication(req)) {
                     if (req.headers && req.headers.authorization) {
                         var config = self.jwtConfig;
@@ -66,6 +79,26 @@ var restify = require('restify');
                     next();
                 }
             }
+
+            function unknownMethodHandler(req, res) {
+                if (req.method.toLowerCase() === 'options') {
+                    var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'Authorization']; // added Origin & X-Requested-With
+
+                    if (res.methods.indexOf('OPTIONS') === -1) {
+                        res.methods.push('OPTIONS');
+                    }
+
+                    res.header('Access-Control-Allow-Credentials', true);
+                    res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
+                    res.header('Access-Control-Allow-Methods', res.methods.join(', '));
+                    res.header('Access-Control-Allow-Origin', req.headers.origin);
+
+                    return res.send(204);
+                }
+                else {
+                    return res.send(new restify.MethodNotAllowedError());
+                }
+            }
             return self;
         };
 
@@ -88,7 +121,6 @@ var restify = require('restify');
             if (needsAuthentication === undefined) {
                 needsAuthentication = self.jwtConfig.allwaysVerifyAuthentication;
             }
-
             var key = path + '_' + httpVerb;
             self.routes[key] = {needsAuthentication: needsAuthentication};
             self.server[httpVerb](path, callback);
